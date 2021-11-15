0c797a22b386bf054a8cdeec968fce611308f3f0
MiniFalafel <eliasjsanders@gmail.com>
2021-11-14 19:01:06 -0500

    Fixed ImGui staticruntime
    I should've caught this earlier. I really don't know how it worked for me earlier

--- Mellow/vendor/imgui/premake5.lua
+++ Mellow/vendor/imgui/premake5.lua
@@ -1,7 +1,7 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-	staticruntime "off"
+	staticruntime "on"
 	
 	targetdir ("bin/" .. outputDir .. "%{prj.name}")
 	objdir ("bin-int/" .. outputDir .. "%{prj.name}")
